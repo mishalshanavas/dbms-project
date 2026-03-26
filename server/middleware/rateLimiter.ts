576408657f3e9b0ff483d4505486b7671898a377
mishal shanavas <mishalshanavas8@gmail.com>
2026-03-26 13:34:53 +0530

    fix: disable rate limiter to fix build

--- server/middleware/rateLimiter.ts
+++ server/middleware/rateLimiter.ts
@@ -1,16 +1,16 @@
-import { RateLimiter } from 'h3-rate-limiter'
+// import { RateLimiter } from 'h3-rate-limiter'
 
-const limiter = new RateLimiter({
-  // 100 requests per 1 minute
-  requests: 100,
-  duration: 60 * 1000,
-  // Ban for 10 minutes
-  ban: 10 * 60 * 1000,
-  // Store in memory
-  driver: {
-    name: 'memory',
-  },
-})
+// const limiter = new RateLimiter({
+//   // 100 requests per 1 minute
+//   requests: 100,
+//   duration: 60 * 1000,
+//   // Ban for 10 minutes
+//   ban: 10 * 60 * 1000,
+//   // Store in memory
+//   driver: {
+//     name: 'memory',
+//   },
+// })
 
 export default defineEventHandler(async (event) => {
   // Don't apply to preflight requests
@@ -21,14 +21,14 @@ export default defineEventHandler(async (event) => {
   if (event.method === 'GET')
     return
 
-  try {
-    await limiter.check(event)
-  }
-  catch (error) {
-    // Set status code to 429
-    event.node.res.statusCode = 429
-    return {
-      error: 'Too many requests',
-    }
-  }
+  // try {
+  //   await limiter.check(event)
+  // }
+  // catch (error) {
+  //   // Set status code to 429
+  //   event.node.res.statusCode = 429
+  //   return {
+  //     error: 'Too many requests',
+  //   }
+  // }
 })
